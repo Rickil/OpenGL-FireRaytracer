42e286b95c6a82fc12ce7785a08d66f551206634
yanis.farhat <yanis.farhat@epita.fr>
2023-06-04 21:20:09 +0200

    adapted fire class to new particle class

--- Fire.hh
+++ Fire.hh
@@ -2,35 +2,35 @@
 #define POGL_FIRE_HH
 
 #include "Particle.hh"
+#include "ObjLoader.hh"
 #include <vector>
+#include <cstdlib>
+
 
 class Fire {
 public:
     std::vector<Particle> particles;
-    std::vector<float> vertex_buffer;
-    std::vector<float> color_buffer;
 
-    void updateBuffers(){
-        vertex_buffer.clear();
-        color_buffer.clear();
 
+    void update(GLuint program_id){
+
+        //update particles buffers
         for (Particle particle : particles){
-            std::vector<float> particle_vertex_buffer = Particle::verticesFromParticle(particle);
-            std::vector<float> particle_color_buffer = {particle.color.x, particle.color.y, particle.color.z};
-
-            vertex_buffer.insert(vertex_buffer.end(), particle_vertex_buffer.begin(), particle_vertex_buffer.end());
-            color_buffer.insert(color_buffer.end(), particle_color_buffer.begin(), particle_color_buffer.end());
+            particle.updateVAO(program_id);
         }
     }
+
 
     Fire(unsigned int nbParticles, Vector3 color){
         for (unsigned int i = 0; i < nbParticles; i++){
-            particles.emplace_back(color);
-        }
-    }
+            Particle particle(color);
 
-    void update(){
-        updateBuffers();
+            //init vao
+            glGenBuffers(1, &(particle.vao));
+
+            //init particles
+            particles.push_back(particle);
+        }
     }
 };
 
